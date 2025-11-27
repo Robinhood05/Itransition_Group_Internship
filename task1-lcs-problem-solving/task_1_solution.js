e06ee85c1bacb9a0a6b6ce601dce213a88fac36d
Sajibul Islam <sirobinsajeeb05@gmail.com>
2025-11-27 08:18:59 +0600

    Add organized project structure with separate problem statements and solutions for each task

---
+++ task1-lcs-problem-solving/task_1_solution.js
@@ -0,0 +1,36 @@
+let a = process.argv.slice(2);
+//base case
+if (a.length === 0) {
+    console.log("");
+    process.exit(0);
+}
+
+//shortest string >= Ans string
+let s = a[0];
+for (let i = 1; i < a.length; i++) {
+    if (a[i].length < s.length) {
+        s = a[i];
+    }
+}
+
+for (let len = s.length; len > 0; len--) {
+    for (let i = 0; i + len <= s.length; i++) {
+
+        let sub = s.substring(i, i + len);
+
+        let ok = true;
+        for (let j = 0; j < a.length; j++) {
+            if (!a[j].includes(sub)) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok) {
+            console.log(sub);
+            process.exit(0);
+        }
+    }
+}
+
+console.log("");
